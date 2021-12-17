bad4c5e73f015ae91a71e9a00683a1eda9cf1bbd
sweetvenom1010 <57639832+sweetvenom1010@users.noreply.github.com>
2021-12-17 22:36:19 +0530

    Add files via upload

--- Django Project/DjangoAPI/MyAPI/ImageProcessing_By_AWS.py
+++ Django Project/DjangoAPI/MyAPI/ImageProcessing_By_AWS.py
@@ -37,6 +37,7 @@ def CelebritiesDetection(imagePath):
     print(imgH,imgW)
 
     response = Service.recognize_celebrities(Image = {"Bytes":image})
+    print(response)
     for objects in response["CelebrityFaces"]:
         CelName = objects["Name"]
         Face = objects["Face"]
@@ -47,10 +48,57 @@ def CelebritiesDetection(imagePath):
         h = int(imgH * box["Height"])
         print(x,y,w,h)
         MyImage = cv2.rectangle(MyImage,(x,y),(x+w, y+h), (0,255,0), 2)
-        MyImage = cv2.putText(MyImage,CelName, (x,y-10),cv2.FONT_HERSHEY_SIMPLEX, 0.9, (90,0,255), 2)
+        MyImage = cv2.putText(MyImage, CelName, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (90, 0, 255), 2)
     cv2.imwrite("detected.jpg", MyImage)
 
 
 
+def facialAnalysis(imagePath):
+    session = boto3.Session(profile_name="default")
+    Service = session.client("rekognition")
+    image = open(imagePath,"rb").read()
+    imgH, imgW = cv2.imread(imagePath).shape[:2]
+    MyImage = cv2.imread(imagePath)
+    print(imgH,imgW)
+    response = Service.detect_faces(Image = {"Bytes":image}, Attributes=['ALL'])
+    print(response)
+    for faceDetail in response['FaceDetails']:
+        Face = faceDetail
+        box = Face["BoundingBox"]
+        x = int(imgW * box["Left"])
+        y = int(imgH * box["Top"])
+        w = int(imgW * box["Width"])
+        h = int(imgH * box["Height"])
+        print(x, y, w, h)
+        MyImage = cv2.rectangle(MyImage, (x-30, y-65), (x + w, y + h), (0, 255, 0), 2)
+        if faceDetail['Landmarks']:
+            for landmarks in faceDetail['Landmarks']:
+                print(str(landmarks['Type']) + "\n")
+
+        age_range = faceDetail['AgeRange']
+        print(" Age Range \nLOW    HIGH" )
+        age_limit = "Age:" + str(age_range['Low']) + "-" + str(age_range['High'])
+        print(str(age_range['Low']) + "\t\t" + str(age_range['High']))
+        print('\n')
+
+        gender = faceDetail['Gender']
+        print("Gender:" + str(gender['Value']))
+        gender = str(gender['Value'])
+        print('\n')
+        MyImage = cv2.putText(MyImage, gender, (x-30, y-65), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (90, 0, 255), 2)
+        MyImage = cv2.putText(MyImage, age_limit, (x-120, y - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (90, 0, 255), 2)
+
+        print('Emotions: \t Confidence\n')
+        for emotion in faceDetail['Emotions']:
+            print(str(emotion['Type']) + '\t\t' + str(emotion['Confidence']))
+            percentage = str(emotion['Confidence'])
+            percentage = percentage[:2] + "%"
+            emote = (str(emotion['Type']) + "-" + percentage)
+            MyImage = cv2.putText(MyImage, emote, (x - 120, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (90, 0, 255), 2)
+            print('\n')
+            break
+    cv2.imwrite("detected.jpg", MyImage)
+
 image = "A:\Django Project\DjangoAPI\MyUploads\image5.jpg"
-CelebritiesDetection(image, "Object Detection")
+facialAnalysis(image)
+#CelebritiesDetection(image)
